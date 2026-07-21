8dc0d9b1dcab4233d6eeaca06135835b2e370e6e
andrew elliott <andrew@andrews-MacBook-Pro.local>
2026-07-21 12:52:59 -0400

    modify argo and add output

--- argo.tf
+++ argo.tf
@@ -2,8 +2,19 @@ data "http" "argo_base_manifest" {
   url = "https://raw.githubusercontent.com/andrewstewartelliott-lang/argocd/main/argo/base/base.yaml"
 }
 
+data "http" "k8s_viewer_cluster_role_manifest" {
+  url = "https://raw.githubusercontent.com/andrewstewartelliott-lang/argocd/main/argo/applications/k8s-viewer/clusterRole.yaml"
+}
+
 resource "kubernetes_manifest" "argo_base" {
   manifest = yamldecode(data.http.argo_base_manifest.response_body)
+
+  depends_on = [kind_cluster.default, helm_release.argocd]
+}
+
+resource "kubernetes_manifest" "k8s_viewer_cluster_role" {
+  description = "The ClusterRole for the k8s-viewer Argo CD application"
+  manifest = yamldecode(data.http.k8s_viewer_cluster_role_manifest.response_body)
 
   depends_on = [kind_cluster.default, helm_release.argocd]
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "kind_cluster_status" {
   description = "Whether the kind cluster has completed provisioning and is ready"
   value       = kind_cluster.default.completed ? "ready" : "not_ready"
 }
+
+output "argocd_helm_message" {
+  description = "The notes returned by the Argo CD Helm release"
+  value       = helm_release.argocd.metadata[0].notes
+}
